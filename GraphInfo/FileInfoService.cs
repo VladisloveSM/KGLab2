2df5f08c01cbd25830db9eaaefeb529730993389
VladisloveSM <80205968+VladisloveSM@users.noreply.github.com>
2022-05-25 22:46:24 +0300

    Lab 2 is added.

---
+++ GraphInfo/FileInfoService.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace GraphInfo;
+
+public sealed class FileInfoService
+{
+    public GraphicalFileInfo GetInfo(string filePath)
+    {
+        var fileInfo = new FileInfo(filePath);
+        var name = fileInfo.Name;
+        var ext = fileInfo.Extension;
+        
+        if (!Enum.TryParse<FileTypes>(ext[1].ToString().ToUpper() + ext[2..].ToLower(), out var fileType))
+        {
+            throw new Exception();
+        }
+
+        var compression = string.Empty;
+
+        switch (fileType)
+        {
+            case FileTypes.Jpg:
+            case FileTypes.Jpeg:
+                compression = "lossy";
+                break;
+            case FileTypes.Bmp:
+                compression = "none";
+                break;
+            case FileTypes.Tif:
+            case FileTypes.Tiff:
+                var img = Image.FromFile(fileInfo.FullName);
+                var compressionTagIndex = Array.IndexOf(img.PropertyIdList, 0x0103);
+                var compressionTag = img.PropertyItems[compressionTagIndex];
+                var com = BitConverter.ToInt16(compressionTag.Value, 0);
+                compression = com + " (tag)";
+                break;
+            case FileTypes.Gif:
+            case FileTypes.Png:
+            case FileTypes.Pcx:
+                compression = "lossless";
+                break;
+        }
+
+        if (fileType == FileTypes.Pcx)
+        {
+            using var myFile = new BinaryReader(File.Open(filePath, FileMode.Open));
+            
+            myFile.BaseStream.Seek(3, SeekOrigin.Begin);
+            var pcxColorDepth = myFile.ReadByte();
+            var xMin = myFile.ReadInt16();
+            var yMin = myFile.ReadInt16();
+            var xMax = myFile.ReadInt16();
+            var yMax = myFile.ReadInt16();
+            var xDpi = myFile.ReadInt16();
+            var yDpi = myFile.ReadInt16();
+
+            return new GraphicalFileInfo(name, xMax - xMin + 1, yMax - yMin + 1, xDpi, yDpi, pcxColorDepth, compression);
+        }
+
+        var bmi = new BitmapImage(new Uri(filePath));
+        var width = bmi.PixelWidth;
+        var height = bmi.PixelHeight;
+        var colorDepth = bmi.Format.BitsPerPixel;
+        var dpiX = Math.Round(bmi.DpiX, 1);
+        var dpiY = Math.Round(bmi.DpiY, 1);
+
+        return new GraphicalFileInfo(name, width, height, dpiX, dpiY , colorDepth, compression);
+    }
+}
---
+++ GraphInfo/MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.Win32;
+
+using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
+
+namespace GraphInfo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly FileInfoService _fileInfoService = new();
+        private List<string> _currentFiles = new List<string>();
+        private List<FileInfoViewModel> _info = new List<FileInfoViewModel>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadFile_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Multiselect = true, 
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                LoadFiles(openFileDialog.FileNames);
+            }
+        }
+
+        private void LoadFolder_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new FolderBrowserDialog();
+
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (!string.IsNullOrEmpty(dialog.SelectedPath))
+                {
+                    var files = Directory.GetFiles(dialog.SelectedPath);
+                    LoadFiles(files);
+                }
+            }
+        }
+
+        private void LoadFiles(IEnumerable<string> files)
+        {
+            _currentFiles = files.ToList();
+            LoadProgress.Maximum = _currentFiles.Count();
+
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.DoWork += WorkerDoWork;
+            worker.ProgressChanged += WorkerProgressChanged;
+            worker.RunWorkerCompleted += WorkerCompleted;
+
+            worker.RunWorkerAsync();
+        }
+
+        private void WorkerDoWork(object? sender, DoWorkEventArgs e)
+        {
+            _info = new List<FileInfoViewModel>();
+            var progress = 0;
+
+            foreach (var fileName in _currentFiles)
+            {
+                var info = _fileInfoService.GetInfo(fileName).ToFileInfoViewModel();
+                _info.Add(info);
+                (sender as BackgroundWorker).ReportProgress(++progress);
+            }
+
+            if (FilesList.ItemsSource != null && FilesList.Items.Count != 0)
+            {
+                _info.AddRange((IEnumerable<FileInfoViewModel>)FilesList.ItemsSource);
+            }
+
+            _info.Sort((x, y) => string.Compare(x.Name, y.Name, StringComparison.Ordinal));
+        }
+
+        private void WorkerProgressChanged(object? sender, ProgressChangedEventArgs e)
+        {
+            LoadProgress.Value = e.ProgressPercentage;
+        }
+
+        private void WorkerCompleted(object? sender, RunWorkerCompletedEventArgs e)
+        {
+            FilesList.ItemsSource = _info;
+            System.Windows.MessageBox.Show("Loading completed!", "Info");
+            LoadProgress.Value = 0;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _info = new List<FileInfoViewModel>();
+            FilesList.ItemsSource = _info;
+        }
+    }
+}
